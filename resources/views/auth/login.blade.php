a1d2fac53ce561bc0b77de8f2254a80d0e12b694
PatelDhara123 <dp78638826@gmail.com>
2025-05-07 14:23:22 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
                     class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                     <div class="container-fluid pe-0 text-center">
                         <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
-                            Dashboard Login
+                            Admin/Distributor/Agent Login
                         </a>
                     </div>
                 </nav>
