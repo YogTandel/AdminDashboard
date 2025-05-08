5754dbf860cbac43a7ac72d7dcb73dc2d7b5fe80
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-08 11:57:39 +0530

    changed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,160 +32,105 @@
             @endif
         </div>
         <!-- End Toast Container -->
-        <div class="container-fluid py-4">
-            <div class="row">
-                <div class="col-lg-6 col-12">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-12">
-                            <div class="card">
-                                <span class="mask bg-primary opacity-10 border-radius-lg"></span>
-                                <div class="card-body p-3 position-relative">
-                                    <div class="row">
-                                        <div class="col-8 text-start">
-                                            <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
-                                                <i class="ni ni-circle-08 text-dark text-gradient text-lg opacity-10"
-                                                    aria-hidden="true"></i>
-                                            </div>
-                                            <h5 class="text-white font-weight-bolder mb-0 mt-3">
-                                                1600
-                                            </h5>
-                                            <span class="text-white text-sm">Users Active</span>
-                                        </div>
-                                        <div class="col-4">
-                                            <div class="dropdown text-end mb-6">
-                                                <a href="javascript:;" class="cursor-pointer" id="dropdownUsers1"
-                                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-h text-white"></i>
-                                                </a>
-                                                <ul class="dropdown-menu px-2 py-3" aria-labelledby="dropdownUsers1">
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Another action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Something else here</a></li>
-                                                </ul>
-                                            </div>
-                                            <p class="text-white text-sm text-end font-weight-bolder mt-auto mb-0">+55%</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-12 mt-4 mt-md-0">
-                            <div class="card">
-                                <span class="mask bg-dark opacity-10 border-radius-lg"></span>
-                                <div class="card-body p-3 position-relative">
-                                    <div class="row">
-                                        <div class="col-8 text-start">
-                                            <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
-                                                <i class="ni ni-active-40 text-dark text-gradient text-lg opacity-10"
-                                                    aria-hidden="true"></i>
-                                            </div>
-                                            <h5 class="text-white font-weight-bolder mb-0 mt-3">
-                                                357
-                                            </h5>
-                                            <span class="text-white text-sm">Click Events</span>
-                                        </div>
-                                        <div class="col-4">
-                                            <div class="dropstart text-end mb-6">
-                                                <a href="javascript:;" class="cursor-pointer" id="dropdownUsers2"
-                                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-h text-white"></i>
-                                                </a>
-                                                <ul class="dropdown-menu px-2 py-3" aria-labelledby="dropdownUsers2">
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Another action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Something else here</a></li>
-                                                </ul>
-                                            </div>
-                                            <p class="text-white text-sm text-end font-weight-bolder mt-auto mb-0">+124%
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-lg-6 col-md-6 col-12">
-                            <div class="card">
-                                <span class="mask bg-dark opacity-10 border-radius-lg"></span>
-                                <div class="card-body p-3 position-relative">
-                                    <div class="row">
-                                        <div class="col-8 text-start">
-                                            <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
-                                                <i class="ni ni-cart text-dark text-gradient text-lg opacity-10"
-                                                    aria-hidden="true"></i>
-                                            </div>
-                                            <h5 class="text-white font-weight-bolder mb-0 mt-3">
-                                                2300
-                                            </h5>
-                                            <span class="text-white text-sm">Purchases</span>
-                                        </div>
-                                        <div class="col-4">
-                                            <div class="dropdown text-end mb-6">
-                                                <a href="javascript:;" class="cursor-pointer" id="dropdownUsers3"
-                                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-h text-white"></i>
-                                                </a>
-                                                <ul class="dropdown-menu px-2 py-3" aria-labelledby="dropdownUsers3">
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Another action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Something else here</a></li>
-                                                </ul>
-                                            </div>
-                                            <p class="text-white text-sm text-end font-weight-bolder mt-auto mb-0">+15%</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-12 mt-4 mt-md-0">
-                            <div class="card">
-                                <span class="mask bg-dark opacity-10 border-radius-lg"></span>
-                                <div class="card-body p-3 position-relative">
-                                    <div class="row">
-                                        <div class="col-8 text-start">
-                                            <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
-                                                <i class="ni ni-like-2 text-dark text-gradient text-lg opacity-10"
-                                                    aria-hidden="true"></i>
-                                            </div>
-                                            <h5 class="text-white font-weight-bolder mb-0 mt-3">
-                                                940
-                                            </h5>
-                                            <span class="text-white text-sm">Likes</span>
-                                        </div>
-                                        <div class="col-4">
-                                            <div class="dropstart text-end mb-6">
-                                                <a href="javascript:;" class="cursor-pointer" id="dropdownUsers4"
-                                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-h text-white"></i>
-                                                </a>
-                                                <ul class="dropdown-menu px-2 py-3" aria-labelledby="dropdownUsers4">
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Another action</a></li>
-                                                    <li><a class="dropdown-item border-radius-md"
-                                                            href="javascript:;">Something else here</a></li>
-                                                </ul>
-                                            </div>
-                                            <p class="text-white text-sm text-end font-weight-bolder mt-auto mb-0">+90%</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+        <div class="row g-3 container-fluid py-4">
+
+            <!-- Card 1 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-primary rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Agent</p>
+                    <h5 class="text-white text-sm">23</h5>
                 </div>
             </div>
-            <x-footer />
+
+            <!-- Card 2 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-info rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Active Agent</p>
+                    <h5 class="text-white text-sm">13</h5>
+                </div>
+            </div>
+
+            <!-- Card 3 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-dark rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Deactive Agent</p>
+                    <h5 class="text-white text-sm">10</h5>
+                </div>
+            </div>
+
+            <!-- Card 4 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-danger rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Chips</p>
+                    <h5 class="text-white text-sm">370520</h5>
+                </div>
+            </div>
+
+            <!-- Card 5 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-primary rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Distributor</p>
+                    <h5 class="text-white text-sm">4</h5>
+                </div>
+            </div>
+
+            <!-- Card 6 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-info rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Active Distributor</p>
+                    <h5 class="text-white text-sm">2</h5>
+                </div>
+            </div>
+
+            <!-- Card 7 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-dark rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Deactive Distributor</p>
+                    <h5 class="text-white text-sm">2</h5>
+                </div>
+            </div>
+
+            <!-- Card 8 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-danger rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Kata</p>
+                    <h5 class="text-white text-sm">310370</h5>
+                </div>
+            </div>
+
+            <!-- Card 9 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-primary rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Player</p>
+                    <h5 class="text-white text-sm">74</h5>
+                </div>
+            </div>
+
+            <!-- Card 10 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-info rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Active Player</p>
+                    <h5 class="text-white text-sm">10</h5>
+                </div>
+            </div>
+
+            <!-- Card 11 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-dark rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Deactive Player</p>
+                    <h5 class="text-white text-sm">64</h5>
+                </div>
+            </div>
+
+            <!-- Card 12 -->
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="p-3 bg-danger rounded">
+                    <p class="text-white font-weight-bolder mb-0">Total Bulk</p>
+                    <h5 class="text-white text-sm">680890</h5>
+                </div>
+            </div>
+
+        </div>
     </main>
 @endsection
