e35ef596137e5dfeecf685df08946b7e7a5d71c1
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-08 09:51:50 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-
 Route::get('/', [AuthController::class, 'showLogin'])->name('show.login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
