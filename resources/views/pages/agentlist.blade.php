c1b1528d94830736823f5b92c4f940dbf3072ae2
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-08 14:38:44 +0530

    changes

--- resources/views/pages/agentlist.blade.php
+++ resources/views/pages/agentlist.blade.php
@@ -12,8 +12,22 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card mb-4">
-                        <div class="card-header pb-0">
-                            <h6>Authors table</h6>
+                        <div class="card-header pb-0 d-flex justify-content-between align-items-center">
+                            <h6>Agent Users</h6>
+                            <div class="d-flex align-items-center gap-3">
+                                <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                                    <div class="input-group">
+                                        <span class="input-group-text text-body"><i class="fas fa-search"
+                                                aria-hidden="true"></i></span>
+                                        <input type="text" class="form-control" placeholder="Search agent...">
+                                        <button class="btn btn-outline-primary mb-0" type="button">Search</button>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn bg-gradient-primary mb-0" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModalAddAgent">
+                                    <i class="fas fa-plus"></i>&nbsp;&nbsp;Add Agent
+                                </button>
+                            </div>
                         </div>
                         <div class="card-body px-0 pt-0 pb-2">
                             <div class="table-responsive p-0">
@@ -82,13 +96,20 @@
                                             </td>
                                             <td class="align-middle">
                                                 <div class="d-flex align-items-center justify-content-around">
-                                                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs me-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Copy Agent">
+                                                    <a href="javascript:;"
+                                                        class="text-secondary font-weight-bold text-xs me-2"
+                                                        data-bs-toggle="tooltip" data-bs-placement="top" title="Copy Agent">
                                                         <i class="fas fa-copy"></i>
                                                     </a>
-                                                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs me-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Agent">
+                                                    <a href="javascript:;"
+                                                        class="text-secondary font-weight-bold text-xs me-2"
+                                                        data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Agent">
                                                         <i class="fas fa-edit"></i>
                                                     </a>
-                                                    <a href="javascript:;" class="text-danger font-weight-bold text-xs toggle-status" data-bs-toggle="tooltip" data-bs-placement="top" title="Block/Unblock Agent">
+                                                    <a href="javascript:;"
+                                                        class="text-danger font-weight-bold text-xs toggle-status"
+                                                        data-bs-toggle="tooltip" data-bs-placement="top"
+                                                        title="Block/Unblock Agent">
                                                         <i class="fas fa-ban"></i>
                                                     </a>
                                                 </div>
@@ -100,8 +121,55 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            {{-- <div class="row">
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalAddAgent" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalAddAgent" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                        <div class="modal-content">
+                            <div class="modal-body p-0">
+                                <div class="card card-plain">
+                                    <div class="card-header pb-0 text-left">
+                                        <h3 class="font-weight-bolder text-primary text-gradient">Add New Agent</h3>
+                                        <p class="mb-0">Enter Agent name and password to register</p>
+                                    </div>
+                                    <div class="card-body pb-3">
+                                        <form role="form text-left">
+                                            <label>Agent Name</label>
+                                            <div class="input-group mb-3">
+                                                <input type="text" class="form-control" placeholder="Name"
+                                                    aria-label="Name" name="name" aria-describedby="name-addon">
+                                            </div>
+                                            <label>Initial Point (RS.0000)</label>
+                                            <div class="input-group mb-3">
+                                                <input type="number" class="form-control" placeholder="0.00"
+                                                    aria-label="Point" name="point" aria-describedby="email-addon">
+                                            </div>
+                                            <label>Password</label>
+                                            <div class="input-group mb-3">
+                                                <input type="password" class="form-control" placeholder="Password"
+                                                    aria-label="Password" name="password"
+                                                    aria-describedby="password-addon">
+                                            </div>
+                                            <label>Confirm Password</label>
+                                            <div class="input-group mb-3">
+                                                <input type="password" class="form-control" placeholder="Password"
+                                                    aria-label="Password" name="password"
+                                                    aria-describedby="password-addon">
+                                            </div>
+                                            <div class="text-center">
+                                                <button type="button"
+                                                    class="btn bg-gradient-primary btn-lg btn-rounded w-100 mt-4 mb-0">Save
+                                                    Agent
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="row">
                 <div class="col-12">
                     <div class="card mb-4">
                         <div class="card-header pb-0">
@@ -356,7 +424,7 @@
                     </div>
                 </div>
             </div> --}}
-            <x-footer />
-        </div>
+                <x-footer />
+            </div>
     </main>
 @endsection
