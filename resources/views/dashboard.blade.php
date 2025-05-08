02ace6a865a4886cc2689eda7aa1a0814102449c
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-08 14:03:00 +0530

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,7 +30,7 @@
             <!-- Card 1 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-primary rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Agent</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Agent</p>
                     <h5 class="text-white text-sm">23</h5>
                 </div>
             </div>
@@ -38,7 +38,7 @@
             <!-- Card 2 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-info rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Active Agent</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Active Agent</p>
                     <h5 class="text-white text-sm">13</h5>
                 </div>
             </div>
@@ -46,7 +46,7 @@
             <!-- Card 3 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-dark rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Deactive Agent</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Deactive Agent</p>
                     <h5 class="text-white text-sm">10</h5>
                 </div>
             </div>
@@ -54,7 +54,7 @@
             <!-- Card 4 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-danger rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Chips</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Chips</p>
                     <h5 class="text-white text-sm">370520</h5>
                 </div>
             </div>
@@ -62,7 +62,7 @@
             <!-- Card 5 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-primary rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Distributor</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Distributor</p>
                     <h5 class="text-white text-sm">4</h5>
                 </div>
             </div>
@@ -70,7 +70,7 @@
             <!-- Card 6 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-info rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Active Distributor</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Active Distributor</p>
                     <h5 class="text-white text-sm">2</h5>
                 </div>
             </div>
@@ -78,7 +78,7 @@
             <!-- Card 7 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-dark rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Deactive Distributor</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Deactive Distributor</p>
                     <h5 class="text-white text-sm">2</h5>
                 </div>
             </div>
@@ -86,7 +86,7 @@
             <!-- Card 8 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-danger rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Kata</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Kata</p>
                     <h5 class="text-white text-sm">310370</h5>
                 </div>
             </div>
@@ -94,7 +94,7 @@
             <!-- Card 9 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-primary rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Player</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Player</p>
                     <h5 class="text-white text-sm">74</h5>
                 </div>
             </div>
@@ -102,7 +102,7 @@
             <!-- Card 10 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-info rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Active Player</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Active Player</p>
                     <h5 class="text-white text-sm">10</h5>
                 </div>
             </div>
@@ -110,7 +110,7 @@
             <!-- Card 11 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-dark rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Deactive Player</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Deactive Player</p>
                     <h5 class="text-white text-sm">64</h5>
                 </div>
             </div>
@@ -118,7 +118,7 @@
             <!-- Card 12 -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="p-3 bg-danger rounded">
-                    <p class="text-white font-weight-bolder mb-0">Total Bulk</p>
+                    <p class="text-white font-weight-bolder mb-3">Total Bulk</p>
                     <h5 class="text-white text-sm">680890</h5>
                 </div>
             </div>
