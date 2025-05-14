9ae286473a360946fd75bec245c2276462d0f4dd
PatelDhara123 <dp78638826@gmail.com>
2025-05-14 13:05:10 +0530

    abc

--- resources/views/pages/distributor.blade.php
+++ resources/views/pages/distributor.blade.php
@@ -193,58 +193,7 @@
             <!-- Add Agent Modal -->
             <div class="modal fade" id="exampleModalAddAgent" tabindex="-1" role="dialog"
                 aria-labelledby="exampleModalAddAgent" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-md" role="document">
-                    <div class="modal-content">
-                        <div class="modal-body p-0">
-                            <div class="card card-plain">
-                                <div class="card-header pb-0 text-left">
-                                    <h3 class="font-weight-bolder text-primary text-gradient">Add New Distributor</h3>
-                                    <p class="mb-0">Enter Distributor name and password to register</p>
-                                </div>
-                                <div class="card-body pb-3">
-                                    <form role="form text-left">
-                                        <label>Name</label>
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Name" aria-label="Name"
-                                                name="name" aria-describedby="name-addon">
-                                        </div>
-                                        <label>Password</label>
-                                        <div class="input-group mb-3">
-                                            <input type="number" class="form-control" placeholder="password" aria-label="Point"
-                                                name="point" aria-describedby="email-addon">
-                                        </div>
-                                        <label>Roll</label>
-                                        <div class="input-group mb-3">
-                                            <input type="password" class="form-control" placeholder="roll"
-                                                aria-label="Password" name="password" aria-describedby="password-addon">
-                                        </div>
-                                        <label>Balance</label>
-                                        <div class="input-group mb-3">
-                                            <input type="password" class="form-control" placeholder="balance"
-                                                aria-label="Password" name="password" aria-describedby="password-addon">
-                                        </div>
-                                        <label>Endpoint</label>
-                                        <div class="input-group mb-3">
-                                            <input type="password" class="form-control" placeholder="endpoint"
-                                                aria-label="Password" name="password" aria-describedby="password-addon">
-                                        </div>
-                                        <label>DateOfCreation</label>
-                                        <div class="input-group mb-3">
-                                            <input type="password" class="form-control" placeholder="Dateofcreation"
-                                                aria-label="Password" name="password" aria-describedby="password-addon">
-                                        </div>
-                                        <div class="text-center">
-                                            <button type="button"
-                                                class="btn bg-gradient-primary btn-lg btn-rounded w-100 mt-4 mb-0">Save
-                                                Distributor
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('pages.creatdistributor')
             </div>
             <!-- End Add Agent Modal -->
 
