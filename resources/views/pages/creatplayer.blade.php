86b652c9e2661b5bce2de60464caa14db904699f
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-15 09:25:31 +0530

    changes

--- resources/views/pages/creatplayer.blade.php
+++ resources/views/pages/creatplayer.blade.php
@@ -1,51 +1,52 @@
 <div class="modal-dialog modal-dialog-centered modal-md" role="document">
-                    <div class="modal-content">
-                        <div class="modal-body p-0">
-                            <div class="card card-plain">
-                                <div class="card-header pb-0 text-left">
-                                    <h3 class="font-weight-bolder text-primary text-gradient">Add New Player</h3>
-                                    <p class="mb-0">Enter Player name and password to register</p>
-                                </div>
-                                <div class="card-body pb-3">
-                                    <form role="form text-left">
-                                    <label>Name</label>
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Name" name="name">
-                                        </div>
-                                        <label>Password</label>
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Password" name="password">
-                                        </div>
-                                        <label>Role</label>
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Role" name="role">
-                                        </div>
-                                        <label>Balance</label>
-                                        <div class="input-group mb-3">
-                                            <input type="number" class="form-control" placeholder="Balance" name="balance">
-                                        </div>
-                                        <label>Distributor</label>
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Distributor" name="distributor">
-                                        </div>
-                                        <label>Agent</label>
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Agent" name="agent">
-                                        </div>
-                                        <label>Winamount</label>
-                                        <div class="input-group mb-3">
-                                            <input type="number" class="form-control" placeholder="Winamount" name="winamount">
-                                        </div>
-                                        <label>Date of Creation</label>
-                                        <div class="input-group mb-3">
-                                            <input type="date" class="form-control" placeholder="Date" name="dateofcreation">
-                                        </div>
-                                        <div class="text-center">
-                                            <button type="submit" class="btn bg-gradient-primary btn-lg w-100 mt-4 mb-0">Save Player</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <div class="modal-content">
+        <div class="modal-body p-0">
+            <div class="card card-plain">
+                <div class="card-header pb-0 text-left">
+                    <h3 class="font-weight-bolder text-primary text-gradient">Add New Player</h3>
+                    <p class="mb-0">Enter Player name and password to register</p>
                 </div>
+                <div class="card-body pb-3">
+                    <form role="form text-left">
+                        <label>Name</label>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Name" name="name">
+                        </div>
+                        <label>Password</label>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Password" name="password">
+                        </div>
+                        <label>Role</label>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Role" name="role">
+                        </div>
+                        <label>Balance</label>
+                        <div class="input-group mb-3">
+                            <input type="number" class="form-control" placeholder="Balance" name="balance">
+                        </div>
+                        <label>Distributor</label>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Distributor" name="distributor">
+                        </div>
+                        <label>Agent</label>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Agent" name="agent">
+                        </div>
+                        <label>Winamount</label>
+                        <div class="input-group mb-3">
+                            <input type="number" class="form-control" placeholder="Winamount" name="winamount">
+                        </div>
+                        <label>Date of Creation</label>
+                        <div class="input-group mb-3">
+                            <input type="date" class="form-control" placeholder="Date" name="dateofcreation">
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn bg-gradient-primary btn-lg w-100 mt-4 mb-0">Save
+                                Player</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
