1e10018c7c1bbec83b0f740773784557d0a5bc58
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-19 10:56:32 +0530

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,27 @@
     <!-- Toast Container -->
     <div class="position-fixed top-0 start-50 translate-middle-x p-3" style="z-index: 1080">
         @if (session('success'))
-            <div class="alert alert-success" role="alert">
-                <strong>{{ session('success') }}</strong>
+            <div class="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive"
+                aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">
+                        {{ session('success') }}
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
             </div>
         @endif
         @if (session('error'))
-            <div class="alert alert-success" role="alert">
-                <strong>{{ session('error') }}</strong>
+            <div class="toast align-items-center text-bg-danger border-0" role="alert" aria-live="assertive"
+                aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">
+                        {{ session('error') }}
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
             </div>
         @endif
     </div>
