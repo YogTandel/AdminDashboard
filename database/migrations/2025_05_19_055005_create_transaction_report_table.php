b397e81818072d4e5906bac560d7a77db148370b
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-19 11:46:35 +0530

    Update 2025_05_19_055005_create_transaction_report_table.php

--- database/migrations/2025_05_19_055005_create_transaction_report_table.php
+++ database/migrations/2025_05_19_055005_create_transaction_report_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('transaction_report', function (Blueprint $table) {
             $table->id();
+            $table->integer('amount');
+            $table->string('from');
+            $table->string('to');
             $table->timestamps();
         });
     }
