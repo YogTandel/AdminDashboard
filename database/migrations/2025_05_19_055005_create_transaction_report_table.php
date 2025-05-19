68dd4d51b1df5db1ad554052d4b368173d39f62b
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-19 12:29:11 +0530

    Update 2025_05_19_055005_create_transaction_report_table.php

--- database/migrations/2025_05_19_055005_create_transaction_report_table.php
+++ database/migrations/2025_05_19_055005_create_transaction_report_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->integer('amount');
             $table->string('from');
             $table->string('to');
-            $table->timestamps();
         });
     }
 
