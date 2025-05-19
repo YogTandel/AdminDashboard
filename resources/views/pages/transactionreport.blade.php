c133e207ed47df35038b78226d95d4313b6dff57
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-19 15:40:33 +0530

    Update transactionreport.blade.php

--- resources/views/pages/transactionreport.blade.php
+++ resources/views/pages/transactionreport.blade.php
@@ -30,7 +30,7 @@
                                         </th>
                                         <th
                                             class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 py-2">
-                                            Transaction ID
+                                            ID
                                         </th>
                                         <th
                                             class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
