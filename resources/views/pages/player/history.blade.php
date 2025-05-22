6e402dc8355ffd7e7171e851eb83f417bad3db0a
Yog Tandel <yogtandel.dev@gmail.com>
2025-05-22 16:25:20 +0530

    Update history.blade.php

--- resources/views/pages/player/history.blade.php
+++ resources/views/pages/player/history.blade.php
@@ -24,24 +24,22 @@
                             </div>
 
                             <!-- Right: Date + Buttons -->
-                            <div class="d-flex flex-wrap align-items-center gap-2 mt-2 mt-md-0">
+                            <div class="d-flex flex-wrap align-items-end gap-2 mt-2 mt-md-0">
                                 <form method="GET" action="{{ route('player.history', '$player->_id') }}">
-                                    <div class="d-flex gap-2 flex-wrap">
+                                    <div class="d-flex justify-content-end gap-2 mb-3">
                                         <div>
                                             <label for="from_date" class="form-label mb-0">From Date</label>
-                                            <input type="date" name="from_date" id="from_date" class="form-control"
+                                            <input type="date" name="from_date" id="from_date" class="form-control mb-0"
                                                 value="{{ request('from_date') }}">
                                         </div>
 
                                         <div>
                                             <label for="to_date" class="form-label mb-0">To Date</label>
-                                            <input type="date" name="to_date" id="to_date" class="form-control"
+                                            <input type="date" name="to_date" id="to_date" class="form-control mb-0"
                                                 value="{{ request('to_date') }}">
                                         </div>
 
-                                        <div class="align-self-end">
-                                            <button type="submit" class="btn btn-sm btn-primary">Filter</button>
-                                        </div>
+                                        <button type="submit" class="btn btn-sm btn-primary mt-4 mb-0">Filter</button>
                                     </div>
 
                                     <!-- NEXT LINE BUTTONS -->
