d68b69441f8d2f7cf69bd305f7920f1921a2a179
Yog Tandel <yogtandel.dev@gmail.com>
2025-06-04 09:53:58 +0530

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -171,6 +171,17 @@
                 </a>
             </li>
 
+            {{-- Transfer Menu --}}
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <div
+                        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="fas fa-exchange-alt text-dark"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Transfer</span>
+                </a>
+            </li>
+
             <!-- Logout -->
             <li class="nav-item">
                 <form action="{{ route('logout') }}" method="POST">
