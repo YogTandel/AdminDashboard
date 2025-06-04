a035d9ad6f346959bfee9459d6ed2437f43fdb23
Yog Tandel <yogtandel.dev@gmail.com>
2025-06-04 17:07:21 +0530

    Update 2025_06_04_111038_create_distributor_report_table.php

--- database/migrations/2025_06_04_111038_create_distributor_report_table.php
+++ database/migrations/2025_06_04_111038_create_distributor_report_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('distributor_report', function (Blueprint $table) {
             $table->id();
+            $table->string('agent_id');
             $table->timestamps();
         });
     }
