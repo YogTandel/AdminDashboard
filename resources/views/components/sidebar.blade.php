690312bf3acf1fe191ac8565976f65d04f4b11ef
PatelDhara123 <dp78638826@gmail.com>
2025-06-06 09:30:04 +0530

    changes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -190,7 +190,7 @@
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fas fa-file-alt text-dark"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Distributor Report</span>
+                    <span class="nav-link-text ms-1">Transfer Report</span>
                 </a>
             </li>
 
