553be89c9b162427bcaf6236534e341c33e3d747
PatelDhara123 <dp78638826@gmail.com>
2025-07-04 12:04:36 +0530

    changes

--- resources/views/pages/livegame.blade.php
+++ resources/views/pages/livegame.blade.php
@@ -144,7 +144,7 @@
                     <div class="card-header pb-0 d-flex justify-content-between align-items-center">
                         <h6 class=" text-dark fw-bolder text-center" style="font-size: 1.5rem;">Live Player</h6>
                     </div>
-                    <div class="card-body px-0 pt-0 pb-2">
+                    <div class="card-body px-0 pt-0 pb-2 mt-2">
                         <div class="table-responsive p-0">
                             <table class="table align-items-center mb-0" style="table-layout: fixed; width: 100%;">
                                 <thead>
