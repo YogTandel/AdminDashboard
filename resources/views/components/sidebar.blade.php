de649c6dcf354e5bd0ff41e7b5ab344b6de50200
PatelDhara123 <dp78638826@gmail.com>
2025-07-14 09:29:39 +0530

    changes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -186,7 +186,7 @@
                 <li class="nav-item">
                     <a class="nav-link {{ Request::is('setting') ? 'active' : '' }}" href="{{ route('setting') }}">
                         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="fas fa-cog text-dark"></i> <!-- Changed icon to cog for settings -->
+                            <i class="fas fa-cog text-dark"></i>
                         </div>
                         <span class="nav-link-text ms-1">Settings</span>
                     </a>
