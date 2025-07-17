00883520d7ebc22397334997ebcddf08e6a0730b
Yog Tandel <yogtandel.dev@gmail.com>
2025-07-17 11:30:27 +0530

    Update Agent.php

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -1,12 +1,11 @@
 <?php
-
 namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model;
 
 class Agent extends Model
 {
-    //
-        protected $fillable = ['balance']; // Include any other fillable fields
+
+    protected $fillable = ['balance'];
 
 }
