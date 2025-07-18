850da0b4b438dc280ff3e942189955d4e18aa94e
PatelDhara123 <dp78638826@gmail.com>
2025-07-18 15:56:04 +0530

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,7 +36,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-primary rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Agent</p>
-                <h5 class="text-white text-sm">23</h5>
+                <h5 class="text-white text-sm">{{ $totalAgents }}</h5>
             </div>
         </div>
 
@@ -44,7 +44,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-info rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Active Agent</p>
-                <h5 class="text-white text-sm">13</h5>
+                <h5 class="text-white text-sm">{{ $activeAgents }}</h5>
             </div>
         </div>
 
@@ -52,7 +52,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-dark rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Deactive Agent</p>
-                <h5 class="text-white text-sm">10</h5>
+                <h5 class="text-white text-sm">{{ $deactiveAgents }}</h5>
             </div>
         </div>
 
@@ -60,7 +60,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-danger rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Chips</p>
-                <h5 class="text-white text-sm">370520</h5>
+                <h5 class="text-white text-sm">{{ $totalChips }}</h5>
             </div>
         </div>
 
@@ -68,7 +68,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-primary rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Distributor</p>
-                <h5 class="text-white text-sm">4</h5>
+                <h5 class="text-white text-sm">{{ $totalDistributors }}</h5>
             </div>
         </div>
 
@@ -76,7 +76,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-info rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Active Distributor</p>
-                <h5 class="text-white text-sm">2</h5>
+                <h5 class="text-white text-sm">{{ $activeDistributors }}</h5>
             </div>
         </div>
 
@@ -84,7 +84,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-dark rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Deactive Distributor</p>
-                <h5 class="text-white text-sm">2</h5>
+                <h5 class="text-white text-sm">{{ $deactiveDistributors }}</h5>
             </div>
         </div>
 
@@ -92,7 +92,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-danger rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Kata</p>
-                <h5 class="text-white text-sm">310370</h5>
+                <h5 class="text-white text-sm">{{ $totalKata }}</h5>
             </div>
         </div>
 
@@ -100,7 +100,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-primary rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Player</p>
-                <h5 class="text-white text-sm">74</h5>
+                <h5 class="text-white text-sm">{{ $totalPlayers }}</h5>
             </div>
         </div>
 
@@ -108,7 +108,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-info rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Active Player</p>
-                <h5 class="text-white text-sm">10</h5>
+                <h5 class="text-white text-sm">{{ $activePlayers }}</h5>
             </div>
         </div>
 
@@ -116,7 +116,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-dark rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Deactive Player</p>
-                <h5 class="text-white text-sm">64</h5>
+                <h5 class="text-white text-sm">{{ $deactivePlayers }}</h5>
             </div>
         </div>
 
@@ -124,7 +124,7 @@
         <div class="col-12 col-sm-6 col-md-3">
             <div class="p-3 bg-danger rounded">
                 <p class="text-white font-weight-bolder mb-3">Total Bulk</p>
-                <h5 class="text-white text-sm">680890</h5>
+                <h5 class="text-white text-sm">{{ $totalBulk }}</h5>
             </div>
         </div>
 
