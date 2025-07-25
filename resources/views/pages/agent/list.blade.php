1cbd873699f53047882625fae94cccf4f6e04771
PatelDhara123 <dp78638826@gmail.com>
2025-07-25 11:28:15 +0530

    changes

--- resources/views/pages/agent/list.blade.php
+++ resources/views/pages/agent/list.blade.php
@@ -77,14 +77,18 @@
                                 <button type="submit" class="btn bg-gradient-warning rounded-pill shadow-sm mb-0">
                                     Search
                                 </button>
+<<<<<<< Updated upstream
                                 @if (request()->has('from_date') ||
                                         request()->has('to_date') ||
                                         request()->has('date_range') ||
                                         request()->has('search'))
                                     <a href="{{ route('agentlist.show') }}"
                                         class="btn btn-secondary btn-sm px-3 mt-3">Reset</a>
+=======
+                                @if (request()->has('search') && request('search') != '')
+                                    <a href="{{ route('agentlist.show') }}" class="btn btn-secondary btn-sm px-3 mt-3">Reset</a>
+>>>>>>> Stashed changes
                                 @endif
-
                             </form>
 
                             <!-- Add Agent -->
