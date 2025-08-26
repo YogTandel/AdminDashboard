eb1bb74509268afc0fef079284f9981dda735370
Yog Tandel <yogtandel.dev@gmail.com>
2025-08-26 16:07:54 +0530

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -63,14 +63,16 @@
             <!-- Agent List -->
             @if (in_array($role, ['admin', 'distributor']))
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('agentlist') ? 'active' : '' }}" href="{{ route('agentlist.show') }}">
+                    <a class="nav-link {{ Request::is('agentlist') ? 'active' : '' }}"
+                        href="{{ route('agentlist.show') }}">
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1"
                                 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                 <title>agents</title>
                                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <g transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                                    <g transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF"
+                                        fill-rule="nonzero">
                                         <g transform="translate(1716.000000, 291.000000)">
                                             <g transform="translate(1.000000, 0.000000)">
                                                 <path class="color-background opacity-6"
@@ -191,7 +193,8 @@
                     href="{{ route('relesecommission-report') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="fas fa-percentage" style="
+                        <i class="fas fa-percentage"
+                            style="
                             color: {{ Request::is('relesecommission-report*') ? '#fb6340' : '#344767' }};
                             transition: color 0.3s ease;">
                         </i>
@@ -205,10 +208,12 @@
                 <a class="nav-link {{ request()->routeIs('Weekly-report') ? 'active' : '' }}"
                     href="{{ route('Weekly-report') }}">
 
-                    <div class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 d-flex align-items-center justify-content-center
+                    <div
+                        class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 d-flex align-items-center justify-content-center
                    {{ request()->routeIs('Weekly-report') ? 'bg-warning text-white' : 'bg-white' }}">
 
-                        <i class="fas fa-calendar-week 
+                        <i
+                            class="fas fa-calendar-week
                    {{ request()->routeIs('Weekly-report') ? 'text-white' : 'text-dark' }}"></i>
                     </div>
 
