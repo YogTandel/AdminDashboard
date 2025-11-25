f1f716558591f1e128d4eb13367da814c66f613a
YogTandel <yogtandel.dev@gmail.com>
2025-11-25 09:27:28 +0530

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
 
     {{-- 🔵 Loader --}}
     <div id="loader"
-        style="
+         style="
     position: fixed;
     top: 0;
     left: 0;
@@ -127,55 +127,6 @@
                     </div>
                 @elseauth('web')
                     @if (auth()->user()->role === 'distributor')
-
-                       {{--  <!-- Card 1 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-primary rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Distributor</p>
-                                <h5 class="text-white text-sm">{{ $totalDistributors }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 2 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-info rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Active Distributor</p>
-                                <h5 class="text-white text-sm">{{ $activeDistributors }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 3 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-dark rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Deactive Distributor</p>
-                                <h5 class="text-white text-sm">{{ $deactiveDistributors }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 1 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-primary rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Player</p>
-                                <h5 class="text-white text-sm">{{ $totalPlayers }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 2 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-info rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Active Player</p>
-                                <h5 class="text-white text-sm">{{ $activePlayers }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 3 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-dark rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Deactive Player</p>
-                                <h5 class="text-white text-sm">{{ $deactivePlayers }}</h5>
-                            </div>
-                        </div> --}}
-
                         <div class="col-12 col-sm-6 col-md-3">
                             <div class="p-3 bg-primary rounded">
                                 <p class="text-white font-weight-bolder mb-3">Total Balance</p>
@@ -183,54 +134,6 @@
                             </div>
                         </div>
                     @elseif(auth()->user()->role === 'agent')
-                        <!-- Card 1 -->
-                        {{-- <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-primary rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Agent</p>
-                                <h5 class="text-white text-sm">{{ $totalAgents }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 2 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-info rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Active Agent</p>
-                                <h5 class="text-white text-sm">{{ $activeAgents }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 3 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-dark rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Deactive Agent</p>
-                                <h5 class="text-white text-sm">{{ $deactiveAgents }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 1 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-primary rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Player</p>
-                                <h5 class="text-white text-sm">{{ $totalPlayers }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 2 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-info rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Active Player</p>
-                                <h5 class="text-white text-sm">{{ $activePlayers }}</h5>
-                            </div>
-                        </div>
-
-                        <!-- Card 3 -->
-                        <div class="col-12 col-sm-6 col-md-4">
-                            <div class="p-3 bg-dark rounded">
-                                <p class="text-white font-weight-bolder mb-3">Total Deactive Player</p>
-                                <h5 class="text-white text-sm">{{ $deactivePlayers }}</h5>
-                            </div>
-                        </div> --}}
-
                         <div class="col-12 col-sm-6 col-md-3">
                             <div class="p-3 bg-primary rounded">
                                 <p class="text-white font-weight-bolder mb-3">Total Balance</p>
@@ -240,27 +143,27 @@
 
                     @endif
                 @endauth
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
 
 
 @section('scripts')
-<script>
-    window.addEventListener('load', function() {
-        setTimeout(() => {
-            const loader = document.getElementById('loader');
-            const content = document.getElementById('mainContent');
-
-            loader.style.opacity = '0';
-
-
+    <script>
+        window.addEventListener('load', function () {
             setTimeout(() => {
-                loader.remove();
-                content.style.display = 'block';
-            }, 500);
-        }, 1000);
-    });
-</script>
+                const loader = document.getElementById('loader');
+                const content = document.getElementById('mainContent');
+
+                loader.style.opacity = '0';
+
+
+                setTimeout(() => {
+                    loader.remove();
+                    content.style.display = 'block';
+                }, 500);
+            }, 1000);
+        });
+    </script>
